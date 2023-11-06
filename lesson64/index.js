515702248164cdc3a7eb8a57d11c1918680c2099
alexcodaline <aleksander.ponom@gmail.com>
2023-11-06 16:50:59 +0200

    lesson64 fix

--- lesson64/index.js
+++ lesson64/index.js
@@ -1,39 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
-import { productSchema } from "./model/product.js";
+
+import { Product, productSchema } from "./model/product.js";
 
 const PORT = 3002;
 
 const app = express();
 
-const url = "mongodb://localhost:27017/shop";
-const connection = mongoose.createConnection(url, { maxPoolSize: 10 });
+const url = "mongodb://localhost:27017";
+const connection = mongoose.createConnection(url, { maxPoolSize: 19 });
 
-const Product = connection.model("product", productSchema);
-
-connection.on("open", () => {
-  console.log("Connected to the database!");
-  app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`DB connection error: ${err}`);
   });
-});
-
-connection.on("error", (err) => {
-  console.error(`Database connection error: ${err}`);
-});
 
 app.get("/", (req, res) => {
   Product.find()
     .then((products) => {
       const productsHtml = products.map(
         (product) => `
-<div style="border: 1px solid #000; 
-width: fit-content; 
-margin: 0 0 20px 0; 
-padding: 0 10px">
-  <h2>${product.title}</h2>
-  <p>Price: ${product.price}</p>
-</div>
+        <div style="border: 1px solid #000;
+        width: fit-content;
+        margin: 0 0 20px 0;
+        padding: 0 10px">
+        <h2>${product.title}</h2>
+        <p>Price: ${product.price}</p>
+        </div>
         `
       );
       const html = `<h1>Products:</h1> ${productsHtml.join("")}`;
